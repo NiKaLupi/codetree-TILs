e46f8f8f01a671c621fd81e100995dd938a6d908
NiKaLupi <countear10@naver.com>
2024-08-13 20:49:03 +0900

    Title: 구간 잘 나누기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240813/구간 잘 나누기/divide-sections-well.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#define MAX_N 100
+
+using namespace std;
+int arr[MAX_N] = {};
+int arr1[MAX_N] = {};
+int main() {
+    int n, m;
+    cin>> n>> m;
+    for(int i = 0; i < n; i++){
+        cin>> arr[i];
+        arr1[i] = arr[i];
+    }
+    int ans = 0;
+    for(int i = 1; i <= 100; i++){
+        int a = 0;
+        int cnt = 0;
+        for(int j = 0; j < n; j++){
+            if(arr[j] > i) break;
+            if(a + arr[j] <= i)
+                a += arr[j]; 
+            else{
+                a = arr[j];
+                cnt++;
+            }
+        }
+        if(cnt == m - 1){
+            ans = i;
+            break;
+        }
+    }
+    cout<< ans;
+    return 0;
+}
