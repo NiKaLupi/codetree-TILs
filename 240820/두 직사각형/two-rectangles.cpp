d6f657288ff79ea4f547544bc30142f9c07debdb
NiKaLupi <countear10@naver.com>
2024-08-20 20:41:14 +0900

    Title: 두 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/두 직사각형/two-rectangles.cpp
+++ 240820/두 직사각형/two-rectangles.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 int main() {
-    int a, b, c, d, a1, b1, c1, d1;
-    cin>> a>> b>> c>> d>> a1>> b1>> c1>> d1;
+    int x1, y1, x2, y2; // 첫 번째 직사각형의 좌표
+    int x3, y3, x4, y4; // 두 번째 직사각형의 좌표
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
 
-    if(b < a1 || d < c1 || b1 < a || d1 < c){
-        cout<< "nonoverlapping";
-    }else{
-        cout<< "overlapping";
+    // 두 직사각형이 겹치는지 확인
+    if (x2 < x3 || x4 < x1 || y2 < y3 || y4 < y1) {
+        cout << "nonoverlapping";
+    } else {
+        cout << "overlapping";
     }
     return 0;
 }
