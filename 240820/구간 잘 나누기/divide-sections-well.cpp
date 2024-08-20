b867e69771f96cb2c367f1ed93e6db63442d3a6c
NiKaLupi <countear10@naver.com>
2024-08-20 19:13:59 +0900

    Title: 구간 잘 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240820/구간 잘 나누기/divide-sections-well.cpp
+++ 240820/구간 잘 나누기/divide-sections-well.cpp
@@ -1,54 +1,36 @@
 #include <iostream>
-#include <algorithm>
-
-using namespace std;
 
 #define MAX_N 100
 
+using namespace std;
+
 int arr[MAX_N] = {};
-
-bool canDivide(int maxSum, int n, int m) {
-    int currentSum = 0, segments = 1;
-
-    for (int i = 0; i < n; ++i) {
-        if (arr[i] > maxSum) {
-            return false; // A single element is greater than maxSum, so division isn't possible.
-        }
-        if (currentSum + arr[i] > maxSum) {
-            ++segments;
-            currentSum = arr[i];
-            if (segments > m) {
-                return false;
-            }
-        } else {
-            currentSum += arr[i];
-        }
-    }
-    return true;
-}
-
+int arr1[MAX_N] = {};
 int main() {
     int n, m;
-    cin >> n >> m;
-
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
+    cin>> n>> m;
+    for(int i = 0; i < n; i++){
+        cin>> arr[i];
+        arr1[i] = arr[i];
     }
-
-    int low = *max_element(arr, arr + n);
-    int high = accumulate(arr, arr + n, 0);
-    int ans = high;
-
-    while (low <= high) {
-        int mid = low + (low - high) / 2;
-        if (canDivide(mid, n, m)) {
-            ans = mid;
-            high = mid - 1;
-        } else {
-            low = mid + 1;
+    int ans = 0;
+    for(int i = 1; i <= 5000; i++){
+        int a = 0;
+        int cnt = 0;
+        for(int j = 0; j < n; j++){
+            //if(arr[j] > i) break;
+            if(a + arr[j] <= i)
+                a += arr[j]; 
+            else{
+                a = arr[j];
+                cnt++;
+            }
+        }
+        if(cnt == m - 1){
+            ans = i;
+            break;
         }
     }
-
-    cout << ans << endl;
+    cout<< ans;
     return 0;
 }
