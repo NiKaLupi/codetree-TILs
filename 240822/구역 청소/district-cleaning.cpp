3e4ad02ad2f60af0dd5e58834a14efd8981412bf
NiKaLupi <countear10@naver.com>
2024-08-22 19:41:51 +0900

    Title: 구역 청소, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240822/구역 청소/district-cleaning.cpp
+++ 240822/구역 청소/district-cleaning.cpp
@@ -7,6 +7,8 @@ int main() {
     cin>> a>> b>> c>> d;
     if(b >= c && b <= d) cout<< d - a;
     else if(d >= a && d <= b) cout<< b - c;
+    else if(c <= a && d >= b) cout<< d - c;
+    else if(c >= a && d <= b) cout<< b - a;
     else cout<< b - a + d - c;
 
     return 0;
