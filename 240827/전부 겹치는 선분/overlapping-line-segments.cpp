4eea34d2bd965900ef17d1ad9730b3bbd9c941a8
NiKaLupi <countear10@naver.com>
2024-08-27 19:20:52 +0900

    Title: 전부 겹치는 선분, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240827/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 100
+int x[MAX_N] = {}, x1[MAX_N] = {};
+int main() {
+    int n;
+    cin>> n;
+
+    int a = 0, b = 100;
+
+
+    bool ans = true;
+    for(int i = 0; i < n; i++){
+        cin>> x[i]>> x1[i]; 
+        if((x[i] >= a && x[i] <= b) || (x1[i] <= b && x1[i] >= a)){ 
+            if(x[i] > a) a = x[i];
+            if(x1[i] < b) b = x1[i];
+        }
+        else ans = false;  
+    }
+    if(ans) cout<< "Yes";
+    else cout<< "No";
+    return 0;
+}
