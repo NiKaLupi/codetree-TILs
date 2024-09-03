59266e09226895b08506008b9114a10c557e027b
NiKaLupi <countear10@naver.com>
2024-09-03 19:16:38 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240903/코딩톡/coding-talk.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_M 100
+#define MAX_N 26
+
+char sender[MAX_M];
+int unreader[MAX_M];
+char users[MAX_N];
+
+int main() {
+    int n, m, p;
+    cin>> n>> m>> p;
+
+    for(int i = 0; i < n; i++){
+        users[i] = 'A' + i;
+    }
+
+    for(int i = 0; i < n; i++){
+        cin>> sender[i]>> unreader[i];    
+    }
+
+    for(int i = p - 1; i < n; i++){
+        users[sender[i] - 'A'] = '0';
+    }
+    if(unreader[p] == unreader[p - 1]) users[sender[p - 1]] = '0'; 
+    if(unreader[p - 1] != 0)  
+        for(int i = 0; i < n; i++){
+            if(users[i] == 'A' + i) cout<< users[i]<< ' ';
+    }
+    return 0;
+}
