aca5dce0e54c0f7814010f9530e9c2c9f1a0be2e
NiKaLupi <countear10@naver.com>
2024-09-03 19:36:17 +0900

    Title: 코딩톡, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240903/코딩톡/coding-talk.cpp
+++ 240903/코딩톡/coding-talk.cpp
@@ -10,24 +10,24 @@ char users[MAX_N];
 
 int main() {
     int n, m, p;
-    cin>> n>> m>> p;
+    cin >> n >> m >> p;
 
     for(int i = 0; i < n; i++){
         users[i] = 'A' + i;
     }
 
     for(int i = 0; i < m; i++){
-        cin>> sender[i]>> unreader[i];    
+        cin >> sender[i] >> unreader[i];    
     }
 
     for(int i = p - 1; i < m; i++){
         users[sender[i] - 'A'] = '0';
     }
-    if(unreader[p - 1] == unreader[p - 2]) users[sender[p - 1] - 'A' + 1] = '0'; 
-    if(unreader[p - 1] != 0)  
-        for(int i = 0; i < n; i++){
-            if(users[i] == 'A' + i) cout<< users[i]<< ' ';
+
+    if (p > 1 && unreader[p - 1] == unreader[p - 2]) {
+        if (sender[p - 1] - 'A' + 1 < n) {
+            users[sender[p - 1] - 'A' + 1] = '0';
+        }
     }
-    //cout<< sender[p - 1] - 'A' + 1;
-    return 0;
-}
+
+    if
