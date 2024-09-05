3998bdff7572316b0e90c739a309bd6ed173eb0f
NiKaLupi <countear10@naver.com>
2024-09-05 19:55:24 +0900

    Title: X 달리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240905/X 달리기/x-run.cpp
+++ 240905/X 달리기/x-run.cpp
@@ -6,23 +6,22 @@ int main() {
     cin >> distance;
 
     int min_time = 10000;
-    for (int i = 0; i < distance; i++) {
-        int speed = 1;
-        int position = 0;
-        int time = 10000;
-        bool acceleration = true;
-        for (int j = 0; j < distance; j++) {
-            if (i == j) acceleration = false;
-            if (acceleration) ++speed;
-            else if(speed > 1)--speed;
-            position += speed;
-            if (position == distance) {
-                time = j + 1;
-                break;
-            }
-        }
+    int max_speed = 1;
+    while(true){
+        int necessary_moved = (max_speed * max_speed); //반드시 {(최고 속도 * 최고 속도 + 1) - 최고 속도} 거리만큼 이동함
+        int time = max_speed * 2 - 1;
+
+        if(necessary_moved > distance) break;
+
+        int have_to_move = distance - necessary_moved;
+        //소요된 시간은 최고 속도 * 2 - 1 + 같은 속도에 머무른 시간
+        if (have_to_move % max_speed != 0)
+            time += have_to_move / max_speed + 1;
+        else
+            time += have_to_move / max_speed;
+        
         if (time < min_time) min_time = time;
-        //cout<< position<< ' ';
+        max_speed++;
     }
     cout << min_time;
     return 0;
