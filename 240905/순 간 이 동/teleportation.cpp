c7469ef738c6ccd3da2c9db29e8f19fe1c06506a
NiKaLupi <countear10@naver.com>
2024-09-05 20:33:54 +0900

    Title: 순 간 이 동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240905/순 간 이 동/teleportation.cpp
+++ 240905/순 간 이 동/teleportation.cpp
@@ -7,8 +7,8 @@ int main() {
     cin>> a>> b>> x>> y;
 
     int pos1 = b - a;
-    int pos2 = abs(x - a) + b - y;
-    int pos3 = abs(y - a) + b - x;
+    int pos2 = abs(x - a) + abs(b - y);
+    int pos3 = abs(y - a) + abs(b - x);
 
     if(pos2 < pos3 && pos2 < pos1) cout<< pos2;
     else if(pos1 < pos2 && pos1 < pos3) cout<< pos1;
