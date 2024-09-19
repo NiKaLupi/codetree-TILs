ccf257ce98b4442d130f30fbee62d14cc126c86e
NiKaLupi <countear10@naver.com>
2024-09-19 20:19:12 +0900

    Title: 독서실의 거리두기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240919/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
+++ 240919/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
@@ -7,41 +7,50 @@ int seat[MAX_N] = {};
 
 int main() {
     int n;
-
     string s;
 
-    cin>> n>> s;
+    cin >> n >> s;
 
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         seat[i] = s[i] - '0';
     }
+
     int x1 = 0, x2 = 0; 
     int max_distance = 0;
-    for(int i = 0; i < n; i++){
-        if(seat[i] == 1){
-            int distance = 0;
-            for(int j = i + 1; j < n; j++){
-                if(seat[j] == 1){ 
-                    distance = j - i;
-                    if(distance > max_distance){
+
+    for (int i = 0; i < n; i++) {
+        if (seat[i] == 1) {
+            for (int j = i + 1; j < n; j++) {
+                if (seat[j] == 1) { 
+                    int distance = j - i;
+                    if (distance > max_distance) {
                         x1 = i, x2 = j;
-                        max_distance = distance / 2 + max_distance % 2 - 1;
-                        break;
+                        max_distance = distance / 2 + distance % 2;
                     }
-                }else if(j - i - 1> max_distance) x1 = i, x2 = j, max_distance = j - i - 1;
+                    break;
+                } else if (j - i > max_distance) {
+                    x1 = i, x2 = j;
+                    max_distance = j - i;
+                }
             }
         }
     }
-    seat[(x1 + x2) / 2 + (x1 + x2) % 2] = 1;
+
+    //cout<< max_distance<< endl;
+    seat[(x1 + x2) / 2] = 1;
+
     int min_dis = 10000;
-    for(int i = 0; i < n - 1; i++){
-        if(seat[i] == 1)    
-            for(int j = i + 1; j < n; j++){
-                if(seat[j] == 1){
-                    if(j - i < min_dis) min_dis = j - i;
+    for (int i = 0; i < n - 1; i++) {
+        if (seat[i] == 1) {
+            for (int j = i + 1; j < n; j++) {
+                if (seat[j] == 1) {
+                    if (j - i < min_dis) min_dis = j - i;
+                    break;
                 }
             }
+        }
     }
-    cout<< min_dis;
+    //cout<< x1<< ' '<< x2;
+    cout << min_dis;
     return 0;
 }
