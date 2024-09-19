9f4bcc26aefacf5c59e6ca6379bffb7858a00c73
NiKaLupi <countear10@naver.com>
2024-09-19 20:51:37 +0900

    Title: 연속된 숫자 만들기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240919/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
+++ 240919/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
@@ -5,6 +5,8 @@ int arr[3] = {};
 int main() {
     cin>> arr[0]>> arr[1]>> arr[2];
     sort(arr, arr + 3);
-    cout<< arr[1] - arr[0] - 1; 
+    if(arr[0] == arr[1] - 1) cout<< arr[2] - arr[1] - 1; 
+    //else if(arr[1] == arr[2] - 1) cout<< arr[2] - arr[1] - 1;
+    else cout<< arr[1] - arr[0] - 1; 
     return 0;
 }
