6b866e24aba85d9a336a2a081c0ed8cf5a17123a
NiKaLupi <countear10@naver.com>
2024-09-24 20:03:43 +0900

    Title: ABC 줄 세우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240924/ABC 줄 세우기/abc-line-up.cpp
+++ 240924/ABC 줄 세우기/abc-line-up.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 using namespace std;
-
 #define MAX_N 26
 char line[MAX_N];
-
 int main() {
     int n;
-    cin >> n;
+    cin>> n;
 
-    for (int i = 0; i < n; i++) {
-        cin >> line[i];
+    for(int i = 0; i < n; i++){
+        cin>> line[i];
     }
 
     int ex_time = 0;
-    for (int i = 0; i < n; i++) {
-        while (line[i] - 'A' != i) {
-            int target_index = line[i] - 'A';
-            swap(line[i], line[target_index]);
-            ex_time++;
+    for(int i = 0; i < n; i++){
+        if(line[i] - 'A' > i){
+            char temp_p = line[i];
+            for(int j = i - 1; j >= 0; j--){
+                line[j + 1] = line[j];
+                line[j] = temp_p;
+                ex_time++;
+                if(line[temp_p - 'A'] == temp_p) break;
+            }
+        }else if(line[i] - 'A' < i){
+            char temp_p = line[i];
+            for(int j = i + 1; j < n; j++){
+                line[j - 1] = line[j];
+                line[j] = temp_p;
+                ex_time++;
+                if(line[temp_p - 'A'] == temp_p) break;
+            }
         }
     }
-
-    cout << ex_time;
+    cout<< ex_time;
     return 0;
 }
