ed5a39ee0e4c830a44f31891596bc0cb4944d1d7
NiKaLupi <countear10@naver.com>
2024-09-26 20:40:11 +0900

    Title: 두 번째로 작은 수의 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240926/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
+++ 240926/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
@@ -7,21 +7,41 @@ int arr[MAX_N];
 
 int main() {
     int n;
-    cin>> n;
+    cin >> n;
 
-    for(int i = 0; i < n; i++){
-        cin>> arr[i];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
     int first_min_num = 101, second_min_num = 101; 
     int index = -1;
-    bool num = false;
-    for(int i = 0; i < n; i++){
-        if(first_min_num > arr[i] ) second_min_num = first_min_num, first_min_num = arr[i], num = false;
-        else if(second_min_num > arr[i] && arr[i] != first_min_num) second_min_num = arr[i], num = false, index = i + 1;
-        else if(second_min_num == arr[i]) num = true;
+    bool duplicate = false;  // 두 번째로 작은 수가 중복되는지 확인하는 변수
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < first_min_num) {
+            // 새로운 최소값을 찾았을 때
+            second_min_num = first_min_num;
+            first_min_num = arr[i];
+            index = i;
+            duplicate = false;  // 새로 찾은 두 번째 값이 중복되지 않음
+        } else if (arr[i] > first_min_num && arr[i] < second_min_num) {
+            // 두 번째로 작은 값을 갱신
+            second_min_num = arr[i];
+            index = i + 1;  // 1-based 인덱스
+            duplicate = false;
+        } else if (arr[i] == second_min_num) {
+            // 두 번째로 작은 값이 중복되는 경우
+            duplicate = true;
+        }
     }
-    if(num || (second_min_num == first_min_num)) cout<< -1;
-    else cout<< index;
+
+    // 두 번째로 작은 값이 없거나 중복된 경우
+    //cout<< first_min_num<< ' '<< second_min_num<<  ' '<< duplicate;
+    if (duplicate || second_min_num == first_min_num) {
+        cout << -1 << endl;
+    } else {
+        cout << index << endl;
+    }
+
     return 0;
 }
