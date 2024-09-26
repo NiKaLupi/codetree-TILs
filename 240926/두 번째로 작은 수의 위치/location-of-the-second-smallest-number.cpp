299528166e85e8764a3d39542c5d9d302bb5d514
NiKaLupi <countear10@naver.com>
2024-09-26 20:45:13 +0900

    Title: 두 번째로 작은 수의 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240926/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
+++ 240926/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
@@ -9,38 +9,37 @@ int main() {
     int n;
     cin >> n;
 
+    if (n < 2) {
+        cout << -1 << endl;
+        return 0;
+    }
+
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    int first_min_num = 101, second_min_num = 101; 
-    int index = -1;
-    bool duplicate = false;  // 두 번째로 작은 수가 중복되는지 확인하는 변수
+    int firstMin = 101, secondMin = 101;
+    int secondMinIndex = -1;
+    bool isDuplicate = false;
 
     for (int i = 0; i < n; i++) {
-        if (arr[i] < first_min_num) {
-            // 새로운 최소값을 찾았을 때
-            second_min_num = first_min_num;
-            first_min_num = arr[i];
-            index = i;
-            duplicate = false;  // 새로 찾은 두 번째 값이 중복되지 않음
-        } else if (arr[i] > first_min_num && arr[i] < second_min_num) {
-            // 두 번째로 작은 값을 갱신
-            second_min_num = arr[i];
-            index = i + 1;  // 1-based 인덱스
-            duplicate = false;
-        } else if (arr[i] == first_min_num) {
-            // 두 번째로 작은 값이 중복되는 경우
-            duplicate = true;
+        if (arr[i] < firstMin) {
+            secondMin = firstMin;
+            firstMin = arr[i];
+            isDuplicate = false;
+        } else if (arr[i] > firstMin && arr[i] < secondMin) {
+            secondMin = arr[i];
+            secondMinIndex = i + 1; // 1-based index
+            isDuplicate = false;
+        } else if (arr[i] == firstMin) {
+            isDuplicate = true;
         }
     }
 
-    // 두 번째로 작은 값이 없거나 중복된 경우
-    //cout<< first_min_num<< ' '<< second_min_num<<  ' '<< duplicate;
-    if (duplicate || second_min_num == first_min_num) {
+    if (isDuplicate || secondMin == firstMin) {
         cout << -1 << endl;
     } else {
-        cout << index << endl;
+        cout << secondMinIndex << endl;
     }
 
     return 0;
