717db90288bcd27f6c8f493fa97b20e8fd93a052
NiKaLupi <countear10@naver.com>
2024-10-01 15:45:36 +0900

    Title: 세 수의 최대 곱, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 241001/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
+++ 241001/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
@@ -3,33 +3,55 @@
 #include <functional>
 using namespace std;
 
+#define MAX_N 1000
+
+int positive[MAX_N] = {};
+int negative[MAX_N] = {};
+
 int main() {
-    int arr1[1000] = {}, arr2[1000] = {};
-    int n, a, i, idx1 = 0, idx2 = 0, min, pl;
+    int n; 
     cin >> n;
-    if(n == 3){
-        cin>> n >> a>> i;
-        cout<< n * a * i;
-        return 0;
-    }
-    for (i = 0; i < n; i++) {
-        cin >> a;
-        if (a >= 0) {
-            arr1[idx1] = a;
-            idx1++;
+    
+    int v;
+
+    if(n == 3) {
+        int val = 1;
+        for(int i = 0; i < n; i++) {
+            cin >> v;
+            val *= v;
         }
-        else {
-            arr2[idx2] = a;
-            idx2++;
+        cout << val;
+    } else {
+        int pl_num = 0, mi_num = 0;
+        for(int i = 0; i < n; i++) {
+            cin >> v;
+            if(v < 0) {
+                negative[mi_num++] = v;
+            } else {
+                positive[pl_num++] = v;
+            }
+        }
+
+        sort(positive, positive + pl_num, greater<int>());
+        sort(negative, negative + mi_num);
+
+        if(mi_num < 2) {
+            // Case with fewer than two negative numbers
+            cout << positive[0] * positive[1] * positive[2];
+        } else if(pl_num >= 2) {
+            // Compare using both positive and negative numbers
+            if(positive[0] * positive[1] * positive[2] > positive[0] * negative[0] * negative[1]) {
+                cout << positive[0] * positive[1] * positive[2];
+            } else {
+                cout << positive[0] * negative[0] * negative[1];
+            }
+        } else if(pl_num == 1) {
+            // If only one positive number is available
+            cout << negative[0] * negative[1] * positive[0];
+        } else {
+            // If no positive numbers are available
+            cout << negative[0] * negative[1] * negative[2];
         }
     }
-    sort(arr1, arr1 + idx1, greater<int>());
-    sort(arr2, arr2 + idx2);
-    min = arr2[0] * arr2[1];
-    pl = arr1[1] * arr1[2];
-    if (min > pl)
-        cout << arr1[0] * min;
-    else
-        cout << arr1[0] * pl;
     return 0;
 }
