6653b6700292119d3a4f519334f2c4121c192dfc
NiKaLupi <countear10@naver.com>
2024-10-10 19:29:05 +0900

    Title: 최소 와이파이 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/최소 와이파이 수/minimum-number-of-wifi.cpp
+++ 241010/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -14,13 +14,14 @@ int main() {
     int  wifi_num = 0;
     for(int i = 0; i < n; i++){
         bool is_connected = false;
-        if(houses[i] == 1) 
+        if(houses[i] == 1){
             for(int j = i; j < i + m; j++)
                 if(wifi[j] == 1) is_connected = true;
             for(int j = i; j >= i - m; j--)
                 if(wifi[j] == 1) is_connected = true;
-        if(!is_connected)  
-            wifi[i + m] = 1, wifi_num++; 
+            if(!is_connected)  
+                wifi[i + m] = 1, wifi_num++; 
+        }
     }
     cout<< wifi_num;
     return 0;
