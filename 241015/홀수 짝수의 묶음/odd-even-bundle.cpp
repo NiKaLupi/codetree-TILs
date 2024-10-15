1ff02c276a3f4719afae75fff30dee354f569f4a
NiKaLupi <countear10@naver.com>
2024-10-15 20:43:28 +0900

    Title: 홀수 짝수의 묶음, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241015/홀수 짝수의 묶음/odd-even-bundle.cpp
+++ 241015/홀수 짝수의 묶음/odd-even-bundle.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
 using namespace std;
+#define MAX_N 1000
+int elements[MAX_N];
 
 int main() {
     int n;
     cin >> n;
-    
+
     int v, odd_num = 0, even_num = 0;
-    
-    // 입력받으면서 짝수와 홀수의 개수를 셈
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cin >> v;
-        if (v % 2 == 0) even_num++;
+        if(v % 2 == 0) even_num++;
         else odd_num++;
     }
-    
-    // 짝수와 홀수 중 더 적은 값이 기본적으로 교대로 배치 가능한 묶음 수의 절반을 차지
-    int max_pairs = min(even_num, odd_num);
-    
-    // 교대가 가능한 최대 묶음 수는 짝수와 홀수가 각각 한 번씩 번갈아 나오는 방식이므로
-    // 기본적으로 `max_pairs * 2`개의 묶음이 가능함
-    int max_groups = max_pairs * 2;
-    
-    // 만약 짝수나 홀수가 남는다면 하나 더 묶을 수 있음
-    if (even_num > odd_num) max_groups++;
-    
-    cout << max_groups << endl;
+
+    if(odd_num - even_num == 1 || odd_num - even_num == -1) 
+        cout << n;
+    else if(odd_num == even_num) 
+        cout << n;
+    else if(n == 2) 
+        cout << odd_num + even_num * 2 - 1;
+    else if(odd_num == 0) 
+        cout << 1;
+    else {
+        if(odd_num == 1) 
+            cout << 3;
+        else if(odd_num < even_num) 
+            cout << odd_num * 2 + 1;
+        else 
+            cout << even_num * 2 + ((odd_num - even_num - 1) / 3) * 2 - 1; // 수정된 부분
+    }
     
     return 0;
 }
