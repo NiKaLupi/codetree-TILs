ecd476d7ec69db5cd406b6e83a1815ba52514b37
NiKaLupi <countear10@naver.com>
2024-10-29 19:29:38 +0900

    Title: A, B, C 찾기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241029/A, B, C 찾기/finding-a-b-c.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int values[7];
+int main() {
+    int a, b, c;
+
+    for(int i = 0; i < 7; i++){
+        cin>> values[i];
+    }
+    sort(values, values + 7);
+
+    a = values[0];
+
+    if(values[1] - a == values[2]) b = values[2];
+    if(values[2] - a == values[1]) b = values[1];
+
+    c = values[6] - a - b;
+
+    cout<< a<< ' '<< b<< ' '<< c;
+    
+    return 0;
+}
