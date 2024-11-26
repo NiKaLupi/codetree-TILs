bcd83bebbfe30348f04aad49b517e5daf6d29b93
NiKaLupi <countear10@naver.com>
2024-11-26 20:20:50 +0900

    Title: 거품 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241126/거품 정렬 구현/implement-bubble-sort.cpp
+++ 241126/거품 정렬 구현/implement-bubble-sort.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-
+#define MAX_N 1000
+int arr[MAX_N];
 int main() {
     int n;
     cin>> n;
-    int arr[n + 1];
     for(int i = 0; i < n; i++){
         cin>> arr[i];
     }
@@ -18,7 +18,7 @@ int main() {
             }
         }
     }
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i < n + 1 ; i++){
         cout<< arr[i]<< ' ';
     }
     return 0;
