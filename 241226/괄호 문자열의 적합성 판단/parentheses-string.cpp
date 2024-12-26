9c61801a78eb4a3dd05388eaef7d4f2390e21054
NiKaLupi <countear10@naver.com>
2024-12-26 20:50:06 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 241226/괄호 문자열의 적합성 판단/parentheses-string.cpp
+++ 241226/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <stack>
+#include <string>
 using namespace std;
 
 int main() {
-    long long long string str;
+    string str;
     cin>> str;
 
     stack<int> s;   
