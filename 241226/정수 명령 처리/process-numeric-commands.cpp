278a116aeaea3a75339180e641159e190ffe1767
NiKaLupi <countear10@naver.com>
2024-12-26 20:19:26 +0900

    Title: 정수 명령 처리, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

--- 241226/정수 명령 처리/process-numeric-commands.cpp
+++ 241226/정수 명령 처리/process-numeric-commands.cpp
@@ -10,10 +10,10 @@ int main() {
     for(int i = 0; i < n; i++){
         cin>> str;
         if(str == "push") cin>> v, s.push(v);
-        if(str == "size") cout<< s.size()<< endl;
-        if(str == "empty") cout<< s.empty()<< endl;
-        if(str == "pop") cout<< s.top()<< endl, s.pop();
-        else cout<< s.top();
+        else if(str == "size") cout<< s.size()<< endl;
+        else if(str == "empty") cout<< s.empty()<< endl;
+        else if(str == "pop") cout<< s.top()<< endl, s.pop();
+        else cout<< s.top()<< endl;
     }
     return 0;
 }
