06c12a439760b27f4d13e97664441c3d91b4cd9d
NiKaLupi <countear10@naver.com>
2025-01-02 19:28:32 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250102/괄호 문자열의 적합성 판단/parentheses-string.cpp
+++ 250102/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -3,44 +3,17 @@
 using namespace std;
 
 int main() {
-    stack<int> stack;
+    stack<int> s;
     string  str;
-    cin>> str;
+    cin>> sit;
     bool finish = false;
     for(int i = 0; i < str.size(); i++){
         if(str[i] == '(') stack.push(1);
-        else if(stack.empty() == true && str[i] == ')'){
-            cout<< "No";
-        if(str[i] == '(') stack.push(1);
-        else if(stack.empty() == true && str[i] == ')'){
-            cout<< "No";
-            finish = true;
-        }else stack.pop();
+        else if(stack.empty == true && str[i] == ')') cout<< "No", finish = true;
+        else stack.pop();
         if(finish) break;
     }
-    if(stack.empty() == false) cout<< "No";
-    else cout<< "Yes";
-    return 0;
-
-
-테스트케이스
-
-
-결과
-
-
-코드 실행
-
-
-제출 및 채점
-
-
-
-            finish = true;
-        }else stack.pop();
-        if(finish) break;
-    }
-    if(stack.empty() == false) cout<< "No";
+    if(stack.empty == false) cout<< "No";
     else cout<< "Yes";
     return 0;
 }
