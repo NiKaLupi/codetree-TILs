cd4e8e5de3017f7a5675ed75bb1dfc1af0cdc089
NiKaLupi <countear10@naver.com>
2025-01-07 20:45:22 +0900

    Title: 수열 조작, Time: 7ms, Memory: 1MB, Status: Passed - CodeTree

--- 250107/수열 조작/sequence-manipulation.cpp
+++ 250107/수열 조작/sequence-manipulation.cpp
@@ -10,9 +10,9 @@ int main() {
 
     deque<int> d;
     for(int i = 0; i < N; i++){
-        d.push_front(i + 1);
+        d.push_back(i + 1);
     }
-    while(d.size() == 1){
+    while(d.size() != 1){
         d.pop_front();
         d.push_back(d.front());
         d.pop_front();
